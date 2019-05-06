81d93d88d297c819d1b1fde3b376a3c8f1347f35
abdirahmat001 <50314069+abdirahmat001@users.noreply.github.com>
2019-05-06 18:15:56 +0700

    Pembuatan form login

---
+++ admin/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id="frm">
+		<form action="index.php" method="POST">
+			<p>
+				<label>Username :</label>
+				<input type="text" name="Username" id="Username"/>
+			</p>
+			<p>
+				<label>Password :</label>
+				<input type="Password" name="Password" id="Password"/>
+			</p>
+			<p>
+				<input type="submit" value="Login" id="btn"/>
+			</p>
+	</form>
+		
+	</div>
+</body>
+</html>
